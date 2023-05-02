ef751aefe1ad78e6417085b4583efb8f34bbf921
Joanna <joanna.x.simms@helsinki.fi>
2023-05-02 15:05:42 +0100

    Rain warning modified so upper limit is now 50 not 30

--- R/CASSIA.R
+++ R/CASSIA.R
@@ -283,7 +283,7 @@ CASSIA <- function(
     if (sum(Tsa < -10) + sum(Tsa > 20) != 0) {warning(paste("Tsa < -10 or Tsa > 20 in year", year, ". Values not impossible, but unlikely check input."))}
     if (sum(Tsb < -10) + sum(Tsb > 20) != 0) {warning(paste("Tsb < -10 or Tsb > 20 in year", year, ". Values not impossible, but unlikely check input."))}
     if (sum(M.soil < 0) + sum(M.soil > 1) != 0) {warning(paste("M.soil < 0 or M.soil > 1 in year", year, ". Values not impossible, but unlikely check input."))}
-    if (sum(Rain < 0) + sum(Rain > 30) != 0) {warning(paste("Rain < 0 or Rain > 30 in year", year, ". Values not impossible, but unlikely check input."))}
+    if (sum(Rain < 0) + sum(Rain > 50) != 0) {warning(paste("Rain < 0 or Rain > 50 in year", year, ". Values not impossible, but unlikely check input."))}
 
     if (sum(is.na(Temp))) {warning(paste("Temp has NA values check input."))}
     if (sum(is.na(PF))) {warning(paste("PF has NA values check input."))}
