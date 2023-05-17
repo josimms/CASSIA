83ff0eb10b433581d3e31549984b7fa61197bbc8
Joanna <joanna.x.simms@helsinki.fi>
2023-05-17 16:27:07 +0300

    Local parameters changed and Glob67 added to gpafill PAR

--- R/Hyytiala_Data_Creation.R
+++ R/Hyytiala_Data_Creation.R
@@ -10,15 +10,9 @@ Hyytiala_Data_Creation <- function(raw.directory,
   # Download the data from the SMEAR database
   ####
 
-  raw.directory = "/home/joanna/Asiakirjat/Hyytiälä/"
-  year_start = 2010
-  year_end = 2022
-
-
   if (download) {
     http.origin = "https://smear-backend.rahtiapp.fi/search/timeseries/csv?tablevariable=HYY_"
     for (variable in c(paste0("META.", c("RH672", "RH1250", "PAR", "CO2168", "T336", "T168", "Precip", "tsoil_5", "tsoil_10", "wsoil_B1", "Glob", "Pamb336")), "EDDY233.GPP")) {
-      variable = paste0("META.", "tsoil_50")
       from = "&from="
       year1 = seq(year_start, year_end, by = 2)
       to = "-01-01T00%3A00%3A00.000&to="
@@ -56,6 +50,7 @@ Hyytiala_Data_Creation <- function(raw.directory,
     GPP$Date <- as.Date(paste(GPP$Year, GPP$Month, GPP$Day, sep = "-"), "%Y-%m-%e")
     Glob <- dplyr::bind_rows(lapply(paste0(raw.directory, list.files(raw.directory, "Glob")), read.csv, dec = "."))
     Glob$Date <- as.Date(paste(Glob$Year, Glob$Month, Glob$Day, sep = "-"), format = "%Y-%m-%d", tz = "BST")
+    CO2168$Date <- as.Date(paste(CO2168$Year, CO2168$Month, CO2168$Day, sep = "-"), format = "%Y-%m-%d", tz = "BST")
 
     all_data <- as.data.frame(cbind(RH672$HYY_META.RH672, RH1250$HYY_META.RH1250, PAR$HYY_META.PAR,
                                     CO2168$HYY_META.CO2168, T336$HYY_META.T336, T168$HYY_META.T168,
@@ -68,7 +63,7 @@ Hyytiala_Data_Creation <- function(raw.directory,
     all_data$Date <- as.Date(paste(RH672$Year, RH672$Month, RH672$Day, sep = "-"), format = "%Y-%m-%d", tz = "BST")
     # Make into daily values!
     all.daily = aggregate(RH672 ~ Date, data = all_data, mean, na.rm = T, na.action = NULL)
-    all.daily = aggregate(cbind(RH1250, CO2168, T336, T168) ~ Date, data = all_data, mean, na.rm = T, na.action = NULL)
+    all.daily.1 = aggregate(cbind(RH1250, CO2168, T336, T168) ~ Date, data = all_data, mean, na.rm = T, na.action = NULL)
     all.daily.2 = aggregate(cbind(tsoil_5, tsoil_10, wsoil_B1, wsoil_B2) ~ Date, data = all_data, mean, na.rm = T, na.action = NULL)
     # µmol m⁻² s⁻¹
     all.daily$GPP = aggregate(HYY_EDDY233.GPP ~ Date, data = GPP, mean, na.rm = T, na.action = NULL)$HYY_EDDY233.GPP
@@ -76,17 +71,20 @@ Hyytiala_Data_Creation <- function(raw.directory,
     # Here the data looks strange for 2017-2018 two years, so made into NA values
     all.daily.sum$Precip[lubridate::year(all.daily.sum$Date) %in% 2017:2018] <- NA
     all.daily.sum$Glob = bigleaf::Rg.to.PPFD(aggregate(HYY_META.Glob~Date, data = Glob, sum, na.rm = T, na.action = NULL)$HYY_META.Glob)
-
+    all.daily.sum$Glob67 = bigleaf::Rg.to.PPFD(aggregate(HYY_META.Glob67~Date, data = Glob, sum, na.rm = T, na.action = NULL)$HYY_META.Glob67)
 
     # PAR data sorting
     PAR$Missing <-is.nan(PAR$HYY_META.PAR)
-    Glob$Missing <-is.nan(Glob$HYY_META.Glob)
+    Glob$MissingGlob <-is.nan(Glob$HYY_META.Glob)
+    Glob$MissingGlob67 <-is.nan(Glob$HYY_META.Glob67)
     PAR$YMD <- as.Date(paste(PAR$Year, PAR$Month, PAR$Day, sep = "-"))
     Glob$YMD <- as.Date(paste(Glob$Year, Glob$Month, Glob$Day, sep = "-"))
     Daily_Missing <- aggregate(Missing~YMD, PAR, sum)
-    Daily_Missing_Glob <- aggregate(Missing~YMD, Glob, sum)
+    Daily_Missing_Glob <- aggregate(MissingGlob~YMD, Glob, sum)
+    Daily_Missing_Glob$MissingGlob67 <- aggregate(MissingGlob67~YMD, Glob, sum)$MissingGlob67
     Daily_Missing$Percentage <- Daily_Missing$Missing/(24*60*60)
-    Daily_Missing_Glob$Percentage <- Daily_Missing_Glob$Missing/(24*60*60)
+    Daily_Missing_Glob$Percentage <- Daily_Missing_Glob$MissingGlob/(24*60*60)
+    Daily_Missing_Glob$Percentage67 <- Daily_Missing_Glob$MissingGlob67/(24*60*60)
     second_multiplier <- function(x) {
       multi <- (24 - 1) * (x) + 1
       return(multi)
@@ -95,10 +93,14 @@ Hyytiala_Data_Creation <- function(raw.directory,
     # Unit correction, µmol m⁻² s⁻¹ to sum mmol m⁻² day-1
     all.daily.sum$PAR <- 0.000001 * 24 * second_multiplier(Daily_Missing$Percentage) * all.daily.sum$PAR
     all.daily.sum$Glob <- 0.000001 * 24 * second_multiplier(Daily_Missing_Glob$Percentage) * all.daily.sum$Glob
+    all.daily.sum$Glob67 <- 0.000001 * 24 * second_multiplier(Daily_Missing_Glob$Percentage67) * all.daily.sum$Glob67
     all.daily.sum$PAR[Daily_Missing$Missing >= 0.75] <- NA
     all.daily.sum$Glob[Daily_Missing_Glob$Missing >= 0.75] <- NA
+    all.daily.sum$Glob67[Daily_Missing_Glob$Missing67 >= 0.75] <- NA
 
-    all.gapfill <- merge(all.daily, all.daily.sum, all = T)
+    all.gapfill.1 <- merge(all.daily, all.daily.sum, all = T)
+    all.gapfill.2 <- merge(all.daily.1, all.daily.2, all = T)
+    all.gapfill <- merge(all.gapfill.1, all.gapfill.2, all = T)
 
     # Gap fill with the other level if possible
     all.gapfill$RH672[is.na(all.gapfill$RH672)] <- all.gapfill$RH1250[is.na(all.gapfill$RH672)] - mean(all.gapfill$RH1250, na.rm = T) + mean(all.gapfill$RH672, na.rm = T)
@@ -111,6 +113,7 @@ Hyytiala_Data_Creation <- function(raw.directory,
     all.gapfill$wsoil_B2[is.na(all.gapfill$wsoil_B2)] <- all.gapfill$wsoil_B1[is.na(all.gapfill$wsoil_B2)] - mean(all.gapfill$wsoil_B1, na.rm = T) + mean(all.gapfill$wsoil_B2, na.rm = T)
     # Units for the Glob value already changed earlier in the code so not changed again here!
     all.gapfill$PAR[is.na(all.gapfill$PAR)] <- all.gapfill$Glob[is.na(all.gapfill$PAR)] - mean(all.gapfill$Glob, na.rm = T) + mean(all.gapfill$PAR, na.rm = T)
+    all.gapfill$PAR[is.na(all.gapfill$PAR)] <- all.gapfill$Glob67[is.na(all.gapfill$PAR)] - mean(all.gapfill$Glob67, na.rm = T) + mean(all.gapfill$PAR, na.rm = T)
 
     # If the gap is smaller than 6 days apply linear interpolation
     all.gapfill[,-1] <- zoo::na.approx(all.gapfill[,-1], na.rm = F, maxgap = 6)
