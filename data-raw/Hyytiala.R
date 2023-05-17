92845c75ba5543be5fa06266fbe1295f22bc7987
Joanna <joanna.x.simms@helsinki.fi>
2023-05-17 16:26:23 +0300

    My directory changed to a variable

--- data-raw/Hyytiala.R
+++ data-raw/Hyytiala.R
@@ -1,6 +1,6 @@
-load(paste0("/home/joanna/Asiakirjat/Hyytiälä/", "Hyde_weather_CASSIA.RData"))
+load(paste0(raw.directory, "Hyde_weather_CASSIA.RData"))
 
-FMI_Data <- lapply(paste0("/home/joanna/Asiakirjat/Hyytiälä/", list.files(path = "/home/joanna/Asiakirjat/Hyytiälä/", pattern = "CASSIA-csv-"))[c(11:12, c(9, 5), c(6, 1), c(13, 10), c(7, 3), c(2, 8), c(14, 4))], read.csv, na.string  = "-")
+FMI_Data <- lapply(paste0(raw.directory, list.files(path = raw.directory, pattern = "CASSIA-csv-"))[c(11:12, c(9, 5), c(6, 1), c(13, 10), c(7, 3), c(2, 8), c(14, 4))], read.csv, na.string  = "-")
 FMI_df <- dplyr::bind_rows(FMI_Data)
 names(FMI_df)[6:7] <- c("Rain", "RH")
 FMI_df$Date <- as.POSIXct(paste(paste(FMI_df$Year, FMI_df$m, FMI_df$d, sep = "-"), FMI_df$Time), format = "%Y-%m-%d %H:%M", tz = "BST")
@@ -10,6 +10,7 @@ FMI_df$YMD <- as.Date(FMI_df$Date)
 FMI_daily <- aggregate(cbind(Rain, RH) ~ YMD, data = FMI_df, mean, na.rm = T, na.action = NULL)
 FMI_daily <- FMI_daily[-nrow(FMI_daily),]
 
+### NOTE CO2168 is downloaded in the Hyytiala_Data_Creation function
 CO2_average <- length(aggregate(HYY_META.CO2168 ~ lubridate::yday(Date), data = CO2168, mean, na.rm = T, na.action = NULL)$HYY_META.CO2168)
 
 data_format$Rain[is.na(data_format$Rain)] <- FMI_daily$Rain[is.na(data_format$Rain)] - mean(FMI_daily$Rain, na.rm = T) + mean(data_format$Rain, na.rm = T)
