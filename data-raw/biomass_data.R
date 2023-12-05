b0d8d25c6e81445d3d30be5a03230754c7b68dea
Joanna Simms <joanna.x.simms@helsinki.fi>
2023-12-05 11:16:03 +0200

    Biomass datasorting

---
+++ data-raw/biomass_data.R
@@ -0,0 +1,25 @@
+direct = "~/Documents/CASSIA_Calibration/Raw_Data/ancillary_ecosystem/"
+
+####
+# Karake
+####
+
+karike_list = lapply(paste0(direct, list.files(path = direct)[2:28]), readxl::read_excel, sheet = "karike")
+karike_list = lapply(karike_list, function(x) {if (sum(c("pvm.", "pvm....2") %in% names(x)) != 0) {
+  names(x)["pvm." == names(x) | "pvm....2" == names(x)] = "pvm"
+  }
+  return(x)})
+
+karike_df_raw_1 = Reduce(function(x, y) merge(x, y, all=TRUE), karike_list[1:6])
+karike_df_raw_1 = karike_df_raw_1[order(karike_df_raw_1$pvm),]
+karike_df_raw_2 = Reduce(function(x, y) merge(x, y, all=TRUE), karike_list[7:12])
+karike_df_raw_2 = karike_df_raw_2[order(karike_df_raw_2$pvm),]
+karike_df_raw_3 = Reduce(function(x, y) merge(x, y, all=TRUE), karike_list[13:18])
+karike_df_raw_3 = karike_df_raw_3[order(karike_df_raw_3$pvm),]
+karike_df_raw_4 = Reduce(function(x, y) merge(x, y, all=TRUE), karike_list[19:24])
+karike_df_raw_4 = karike_df_raw_4[order(karike_df_raw_4$pvm),]
+karike_df_raw_5 = Reduce(function(x, y) merge(x, y, all=TRUE), karike_list[25:27])
+karike_df_raw_5 = karike_df_raw_5[order(karike_df_raw_5$pvm),]
+karike_df_list = list(karike_df_raw_1, karike_df_raw_2, karike_df_raw_3, karike_df_raw_4, karike_df_raw_5)
+
+karike_df_all = dplyr::bind_rows(dplyr::bind_rows(dplyr::bind_rows(dplyr::bind_rows(karike_df_raw_1, karike_df_raw_2), karike_df_raw_3), karike_df_raw_4), karike_df_raw_5)
