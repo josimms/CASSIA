df1d6244f80d5804f14b40375fe31300a2f34116
Joanna Simms <joanna.x.simms@helsinki.fi>
2024-02-05 15:12:29 +0200

    Parameters added for the nitrogen tests, although the values aren't the final values

--- R/test.R
+++ R/test.R
@@ -7,39 +7,121 @@ tests <- function() {
   respiration_graphs(Rm, Q10)
 
   ## NH4 version
-  nitrogen_graphs(NH4, Temp, N_limits_plant[1], N_k_plant[1], SoilWater, SWC_sat_plant[1])
+  NH4 = 0.3
+  NO3 = 0.03
+  Norg = 0.3
+  Temp = 15
+  Tsb = 5
+  SoilWater = 0.5
+  N_limits_plant = c(0.3, 0.3, 0.3)
+  N_k_plant = c(10, 10, 10)
+  SWC_sat_plant = c(0.2, 0.2, 0.2)
+  nitrogen_graphs(NH4, Temp, SoilWater, N_limits_plant[1], N_k_plant[1], SWC_sat_plant[1])
 
-  N_allo = 0.1
-  max_C_allocation_CASSIA = 0.1
+  C_roots = 0.03
+  N_roots = 0.03
+  C_fungal = 0.03
+  N_fungal = 0.03
+  optimal_root_fungal_biomass_ratio = 0.5
+  NC_fungal_opt = 0.5
   decision_graphs(C_roots, N_roots, C_fungal, N_fungal,
                   optimal_root_fungal_biomass_ratio,
-                  N_allo, max_C_allocation_CASSIA,
-                  NC_fungal_opt, growth_C, growth_N)
+                  NC_fungal_opt)
 
+  growth_C = 0.3
+  growth_N = 0.3
+  C_fungal_in = 5
+  N_fungal_in = 5
   myco_growth_graphs(C_fungal_in, N_fungal_in, growth_C, growth_N)
 
+  micorization = 0.9
+  demand = 1
+  NH4_on_NO3 = 0.3
   nitrogen_graphs_plant(Temp, SoilWater, micorization, NH4, NO3, Norg,
-                        N_limits_plant, N_k_plant, SWC_sat_plant, c(Rm, Q10), 1)
+                        N_limits_plant, N_k_plant, SWC_sat_plant, NH4_on_NO3, demand)
+
+  N_limits_fungal = c(0.3, 0.3, 0.3)
+  N_k_fungal = c(10, 10, 10)
+  SWC_sat_fungal = c(0.2, 0.2, 0.2)
   nitrogen_graphs_fungal(Temp, SoilWater,
                          NH4, NO3, Norg,
                          N_limits_fungal, N_k_fungal,
-                         SWC_sat_fungal, 1)
+                         SWC_sat_fungal, demand)
+  # TODO: not sure that this is doing the right thing! Could be the graphs that are being printed!
 
+  C_microbe = 26
+  N_microbe = 26
+  C_SOM = 50
+  C_FOM = 26
+  NC_microbe_opt = 0.5
+  NC_Litter = 0.8
+  imobilisation = 0.2
+  assimilation = 0.2
+  N_limits_microbes = c(0.3, 0.3, 0.3)
+  N_k_microbes = c(10, 10, 10)
+  SWC_sat_microbes = c(0.2, 0.2, 0.2)
   nitrogen_graphs_microbe(C_microbe, N_microbe, C_SOM, NC_microbe_opt, NH4, NO3, Norg,
                           Temp, SoilWater, NC_Litter, imobilisation, assimilation,
                           N_limits_microbes, N_k_microbes, SWC_sat_microbes,
-                          TRUE, Rm, Q10)
+                          TRUE)
   nitrogen_graphs_microbe(C_microbe, N_microbe, C_FOM, NC_microbe_opt, NH4, NO3, Norg,
                           Temp, SoilWater, NC_Litter, imobilisation, assimilation,
                           N_limits_microbes, N_k_microbes, SWC_sat_microbes,
-                          FALSE, Rm, Q10)
+                          FALSE)
 
-  mycofon_balence_graphs(C_roots, N_roots, optimal_root_fungal_biomass_ratio,
-                         C_fungal, N_fungal, turnover_roots, turnover_roots_mycorrhized, turnover_mantle, turnover_ERM,
-                         Rm, Q10, NH4, NO3, Norg, NC_in_fungai_opt, Temp, Tsb, SoilWater,
-                         N_limits_plant, N_k_plant, SWC_sat_plant, N_limits_fungal, N_k_fungal, SWC_sat_fungal,
-                         mantle_mass, ERM_mass, NH4_on_NO3, growth_C, growth_N,
-                         max_C_allocation_CASSIA, to_CASSIA, TRUE)
+
+  parameters_R = c(0.016906, # microbe_turnover (Preveen, 2013)
+                   (0.001*2875)/1881, # NC_in_root_opt (Heimisaari, 1995)
+                   0.025, # NC_fungal_opt (Meyer, 2010)
+                   1/28.73, # NC_microbe_opt (Heinonsalo, 2015)
+                   0.5, # percentage_C_biomass (CASSIA)
+                   # fungal
+                   0.3, 0.3, 0.3, # N_limits: NH4, NO3, Norg
+                   50, 50, 50, # N_k: NH4, NO3, Norg
+                   0.2, 0.2, 0.2, # SWC_limits: NH4, NO3, Norg
+                   # plant
+                   0.3, 0.3, 0.3, # N_limits: NH4, NO3, Norg
+                   50, 50, 50, # N_k: NH4, NO3, Norg
+                   0.2, 0.2, 0.2, # SWC_limit: NH4, NO3, Norg
+                   # microbes
+                   0.3, 0.3, 0.3, # N_limits: NH4, NO3, Norg
+                   50, 50, 50, # N_k: NH4, NO3, Norg
+                   0.2, 0.2, 0.2, # SWC_limit: NH4, NO3, Norg
+                   0.3, 50, 0.2, # C limits
+                   10, # NH4_on_NO3
+                   0.9, # optimal_root_fungal_biomass_ratio (TODO: Heinonsalo?)
+                   1/625, # turnover_mantle, Meyer, 2010
+                   1/50, # turnover_ERM, Meyer 2010
+                   1/365, # turnover_roots, Meyer, 2010
+                   1/625, # turnover_roots_mycorrhized Meyer, 2010
+                   0.2, # turnover_fungal TODO: do I need this if the turnover is in Meyer?
+                   1, # mantle_mass
+                   1, # ERM_mass
+                   0.03, # growth_C (Franklin, 2017)
+                   0.03, # growth_N (TODO)
+                   1, # C_value_param_myco
+                   1, # N_value_param_myco
+                   1, # C_value_param_plant
+                   1) # N_value_param_plant
+  C_roots_NonStruct = 0.03
+  N_roots_NonStruct = 0.03
+  C_fungal_NonStruct = 0.03
+  N_fungal_NonStruct = 0.03
+  max_C_allocation_CASSIA = 0.3
+  mycofon_balence_graphs(C_roots, N_roots, C_fungal, N_fungal,
+                         C_roots_NonStruct, N_roots_NonStruct, C_fungal_NonStruct, N_fungal_NonStruct,
+                         max_C_allocation_CASSIA,
+                         parameters_R,
+                         NH4, NO3, Norg,
+                         Temp, Tsb, SoilWater,
+                         TRUE)
+  mycofon_balence_graphs(C_roots, N_roots, C_fungal, N_fungal,
+                         C_roots_NonStruct, N_roots_NonStruct, C_fungal_NonStruct, N_fungal_NonStruct,
+                         max_C_allocation_CASSIA,
+                         parameters_R,
+                         NH4, NO3, Norg,
+                         Temp, Tsb, SoilWater,
+                         FALSE)
 
   Litter_mantle = 1
   Litter_ERM = 1
@@ -61,8 +143,5 @@ tests <- function() {
                                    SOM_Norg_used,
                                    Rm, Q10, N_limits_microbes, N_k_microbes, SWC_sat_microbes,
                                    NC_microbe_opt, microbe_turnover)
-  ###
-  # Check that there is mass conservation
-  ###
 
 }
