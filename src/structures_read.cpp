743a6a8c76360cbc16221bc3d59735c22fec19b1
Joanna Simms <joanna.x.simms@helsinki.fi>
2024-08-20 00:11:30 +0200

    form factor readded into the parameters being read
    column name checker also added, but commented

--- src/structures_read.cpp
+++ src/structures_read.cpp
@@ -17,9 +17,6 @@ void printColumnNames(Rcpp::DataFrame df) {
 
 CASSIA_parameters make_CASSIA_parameters(Rcpp::DataFrame input_parameters,
                                          Rcpp::DataFrame input_sperling) {
-
-  printColumnNames(input_parameters);
-
   CASSIA_parameters out;
   std::vector<double> temp = input_parameters["Q10.N"];
   out.Q10_N = temp[0];
@@ -457,7 +454,11 @@ CASSIA_ratios read_ratios(const std::string& filename, const std::string& site) 
 
 
 CASSIA_ratios make_ratios(Rcpp::DataFrame input) {
+  // printColumnNames(input);
+
   CASSIA_ratios out;
+  std::vector<double> temp = input["form_factor"];
+  out.form_factor = temp[0];
   std::vector<double> temp1 = input["needle_fineroot_ratio"];
   out.needle_fineroot_ratio = temp1[0];
   std::vector<double> temp2 = input["sapwood.share"];
