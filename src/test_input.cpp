58a2e7cc2cc8cd9dbb13846352a54ca7097b2a42
Joanna Simms <joanna.x.simms@helsinki.fi>
2024-08-19 19:38:47 +0200

    Everything commented, but will have the tests for the parameters

---
+++ src/test_input.cpp
@@ -0,0 +1,56 @@
+#include "CASSIA.h"
+
+/*
+int CASSIA_ratios_test(std::string fratios, std::string site) {
+  // Example usage: read values for the 'Hyde' site
+  CASSIA_ratios hyde_ratios = read_ratios(fratios, site);
+
+  // Output the read values
+  std::cout << "Hyde Ratios:" << std::endl;
+  std::cout << "Form Factor: " << hyde_ratios.form_factor << std::endl;
+  std::cout << "Needle Fine Root Ratio: " << hyde_ratios.needle_fineroot_ratio << std::endl;
+  std::cout << "Sapwood Share: " << hyde_ratios.sapwood_share << std::endl;
+  std::cout << "Height Growth Coefficient: " << hyde_ratios.height_growth_coefficient << std::endl;
+  std::cout << "Diameter Growth Coefficient: " << hyde_ratios.diameter_growth_coefficient << std::endl;
+  std::cout << "Height Growth Coefficient Max: " << hyde_ratios.height_growth_coefficient_max << std::endl;
+  std::cout << "Height Growth Coefficient Min: " << hyde_ratios.height_growth_coefficient_min << std::endl;
+  std::cout << "Diameter Growth Coefficient Max: " << hyde_ratios.diameter_growth_coefficient_max << std::endl;
+  std::cout << "Diameter Growth Coefficient Min: " << hyde_ratios.diameter_growth_coefficient_min << std::endl;
+
+  return 0;
+}
+
+
+int CASSIA_common_test(std::string fcommon) {
+  CASSIA_common common_params = read_common_parameters(fcommon);
+
+  // Output the read values
+  std::cout << "CASSIA Common Parameters:" << std::endl;
+  std::cout << "a: " << std::setprecision(16) << common_params.a << std::endl;
+  std::cout << "b: " << common_params.b << std::endl;
+  std::cout << "TR0: " << common_params.TR0 << std::endl;
+  std::cout << "abs_zero: " << common_params.abs_zero << std::endl;
+  std::cout << "b_s: " << common_params.b_s << std::endl;
+  std::cout << "theetta_FC: " << common_params.theetta_FC << std::endl;
+  std::cout << "phi_e: " << common_params.phi_e << std::endl;
+  std::cout << "K_sat: " << common_params.K_sat << std::endl;
+  std::cout << "R_length: " << common_params.R_length << std::endl;
+  std::cout << "M_H20: " << common_params.M_H20 << std::endl;
+  std::cout << "r_cyl: " << common_params.r_cyl << std::endl;
+  std::cout << "r_root: " << common_params.r_root << std::endl;
+  std::cout << "ypsilon: " << common_params.ypsilon << std::endl;
+  std::cout << "Rg_N: " << common_params.Rg_N << std::endl;
+  std::cout << "Rg_S: " << common_params.Rg_S << std::endl;
+  std::cout << "Rg_R: " << common_params.Rg_R << std::endl;
+  std::cout << "gas_const: " << common_params.gas_const << std::endl;
+  std::cout << "M_C: " << common_params.M_C << std::endl;
+  std::cout << "M_H: " << common_params.M_H << std::endl;
+  std::cout << "M_O: " << common_params.M_O << std::endl;
+  std::cout << "osmotic_sugar_conc: " << common_params.osmotic_sugar_conc << std::endl;
+  std::cout << "m_N: " << common_params.m_N << std::endl;
+  std::cout << "Uggla: " << common_params.Uggla << std::endl;
+
+  return 0;
+}
+
+*/
