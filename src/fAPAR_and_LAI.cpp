e3466a5705262f710b4194da820d54b7f1057d40
Joanna Simms <joanna.x.simms@helsinki.fi>
2025-11-11 18:52:15 +0200

    fS_reached_one and fN_reached_one added into the dyanmics of the leaf mass and sapwood mass. To do this the culmative gneedle growth is divided by last year's maximum for the increase and then the fS modifier from preles is used for the senesecence. Also the multipliers are made to just stay as 1 or start at 1 so the factors can just be added. This makdes the growing seasons more dyanmic.

--- src/fAPAR_and_LAI.cpp
+++ src/fAPAR_and_LAI.cpp
@@ -5,6 +5,9 @@ void compute_fAPAR_used(int day,
                         int days_gone,
                         double LAI,
                         double max_needles,
+                        bool& fS_reached_one,
+                        bool& fN_reached_one,
+                        repola_out repola,
                         const Settings& boolsettings,
                         const CASSIA_parameters& parameters,
                         const growth_state& tree_state,
@@ -12,22 +15,47 @@ void compute_fAPAR_used(int day,
                         const weather_all& climate,
                         output_vector& all_out
 ) {
+
+  /*
+   * Indexes
+   */
+
+  int index_ref = days_gone + day - 1;
+  if (index_ref < 0) {
+    index_ref = 0;
+  }
+
+  /*
+   * Parameters
+   */
+
   double fAPAR_used = 0.0;
   double LAI_within_year = 0.0;
+  double LMA = 0.086; // kg / m-2
+  double leaf_to_sapwood = 1.0 / 1400.0; // m2 m-2
+  double needle_mass = repola.needle_mass; // TODO units?
+  double needle_mass_sapwood = repola.needle_mass;
+
+  // Logging
+  all_out.LAI[days_gone + day] = LAI_within_year;
+  all_out.fAPAR[days_gone + day] = fAPAR_used;
+
+  // Tian 2021 method
+  double f_modifer = all_out.culm_growth.needles[index_ref]/max_needles;
+  if (f_modifer > 0.99) fN_reached_one = true;
+  if (fN_reached_one) f_modifer = 1.0;
+
+  double senescence = 0.0;
+  // Day 50 added below as the first few days can give 1 which ruins the logic!
+  if (photosynthesis.fS >= 1.0 && day > 50) fS_reached_one = true;
+  if (fS_reached_one) senescence = 1.0 - photosynthesis.fS;
+
+  // Calculated first as needle_mass changed otherwise
+  needle_mass_sapwood = needle_mass * (parameters.n_age - 1.0) / parameters.n_age + (1.0 / parameters.n_age) * f_modifer * needle_mass;
+  needle_mass = needle_mass * (parameters.n_age - 1.0) / parameters.n_age + (1.0 / parameters.n_age) * f_modifer * needle_mass - (1.0 / parameters.n_age) * senescence * needle_mass;
 
   if (!boolsettings.photosynthesis_as_input && boolsettings.fAPAR_Tian) {
-    // Tian 2021 method
-    double f_modifer = (max_needles == 0.0) ? 0.0 : tree_state.needles / max_needles;
-
-    if (day < 182) {
-      LAI_within_year = LAI * (parameters.n_age - 1) / parameters.n_age +
-        (1.0 / parameters.n_age) * f_modifer * LAI;
-    } else if (day > 244) {
-      LAI_within_year = LAI * (parameters.n_age - 1) / parameters.n_age +
-        (1.0 / parameters.n_age) * photosynthesis.fS * LAI;
-    } else {
-      LAI_within_year = LAI;
-    }
+    LAI_within_year = LAI * (parameters.n_age - 1.0) / parameters.n_age + (1.0 / parameters.n_age) * f_modifer * LAI - (1.0 / parameters.n_age) * senescence * LAI;
 
     fAPAR_used = 1.0 - std::exp(-0.52 * LAI_within_year);
 
@@ -47,30 +75,30 @@ void compute_fAPAR_used(int day,
     fAPAR_used = 0.0;
   }
 
-  // Logging
+  /*
+   * Logging
+   */
+
+  // Leaf
+
   all_out.LAI[days_gone + day] = LAI_within_year;
   all_out.fAPAR[days_gone + day] = fAPAR_used;
 
-  /*
-   *  Xylem and phloem growth from leaf foliage
-   */
-  int index_ref = days_gone + day - 1;
-  if (index_ref < 0) {
-    index_ref = 0;
-  }
+  all_out.culm_growth.leaf_mass[day + days_gone] = needle_mass;
+  all_out.culm_growth.leaf_area[day + days_gone] = needle_mass_sapwood / LMA;
 
-  double LMA = 0.086; // kg / m-2
-  double leaf_to_sapwood = 1.0/1400; // 0.0075; // m2 m-2
-
-  all_out.culm_growth.leaf_mass[day + days_gone] = repola_values.needle_mass;
-  all_out.culm_growth.leaf_area[day + days_gone] = repola_values.needle_mass / LMA;
   double sapwood_area = leaf_to_sapwood * all_out.culm_growth.leaf_area[day + days_gone];
+
+  // Wood growth
+
   // (Scheistl Aalto, 2019): Mean wood density 200 kg C m−3
-  all_out.culm_growth.sapwood[day + days_gone] = sapwood_area * all_out.culm_growth.height[index_ref] * 200;
+  all_out.culm_growth.sapwood[day + days_gone] = sapwood_area * all_out.culm_growth.height[index_ref] * 200.0;
 
-  // (Scheistl Aalto, 2019): "Sapwood was further divided to 1) smaller branches and 2) bigger branches and truck with ratio 1/9"
-  all_out.culm_growth.xylem_sh[day + days_gone] = 1.0/9.0 * all_out.culm_growth.sapwood[day + days_gone];
-  all_out.culm_growth.xylem_st[day + days_gone] = 8.0/9.0 * all_out.culm_growth.sapwood[day + days_gone];
-  all_out.culm_growth.phloem[day + days_gone]   = 0.1 * all_out.culm_growth.sapwood[day + days_gone];
+  // (Scheistl Aalto, 2019): "Sapwood was further divided to
+  // 1) smaller branches and 2) bigger branches and trunk with ratio 1/9"
+  all_out.culm_growth.xylem_sh[day + days_gone] = (1.0 / 9.0) * all_out.culm_growth.sapwood[day + days_gone];
 
+  all_out.culm_growth.xylem_st[day + days_gone] = (8.0 / 9.0) * all_out.culm_growth.sapwood[day + days_gone];
+
+  all_out.culm_growth.phloem[day + days_gone] = 0.1 * all_out.culm_growth.sapwood[day + days_gone];
 }
--- src/repola.cpp
+++ src/repola.cpp
@@ -1,6 +1,6 @@
 #include "CASSIA.h"
 
-repola_out repola(CASSIA_parameters parameters) {
+repola_out repola(double diameter, double height, CASSIA_parameters parameters) {
 
   /*
    * parameters.b0_repo = -6.303;
@@ -10,17 +10,14 @@ repola_out repola(CASSIA_parameters parameters) {
   parameters.n_age = 3;
    */
 
-  double diameter = parameters.D0*100;
-  double height = parameters.h0;
-
-  double dski_repo = 2 + 1.25*diameter;
+  double dski_repo = 2 + 1.25*diameter*100;
 
   // NOTE: needle mas could be an input
   double needle_mass = exp(parameters.b0_repo+parameters.b1_repo*dski_repo/(dski_repo+6) + parameters.b2_repo*height/(height+1));
   double m_N_tot = needle_mass*parameters.carbon_share;		// kg C / tree
   double m_N = m_N_tot/(parameters.n_age*parameters.n_length);				// youngest needles (kg C / mm needle)
 
-  double m_R_tot = m_N_tot*0.5;				// kg C / tree
+  double m_R_tot = m_N_tot*0.5;				// kg C / tree // TODO: is this the roots?
   // VARIO changes from original code *0.7 # kg C / tree, keksitty!
 
   repola_out out;
